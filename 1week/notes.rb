4dca06106580fe678befb161ff2f2d278e1777aa
Pradeep Gupta <pradeepb2b@yahoo.com>
2012-10-28 20:13:30 +0530

    mend

---
+++ 1week/notes.rb
@@ -0,0 +1,5 @@
+#>> 1.class.ancestors
+#=> [Fixnum, Integer, Numeric, Comparable, Object, Kernel, BasicObject]
+#
+#By this type we can know the ancestors class.
+#Here Fixnum is class of 1 object and Integers is superclass of Fixnum, Comparable is method of Integre class and Numeric is class of comparable method, Kernel is also method here and BasicObject is Superclass
