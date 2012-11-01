f03b7a562d98038c102de9946d9516d586c2f82e
Pradeep Gupta <pradeepb2b@yahoo.com>
2012-11-01 17:36:06 +0530

    Started to Solve week 1 Excersie

---
+++ 1week/w1e05.rb
@@ -0,0 +1,9 @@
+#Exercise 5. The following program prints the value of the variable. Why?
+
+
+my_string = 'Hello Ruby World'
+def my_string
+  'Hello World'
+end
+puts my_string
+#Answer: Here puts give us first string so in this case first string it got is my_string, So thats the reason it prints the value of variable.
---
+++ ruby_studio/variable.rb
@@ -0,0 +1,4 @@
+movies = "ghostbusters"
+title = movies.capitalize.ljust(30,'.')
+rank = 8
+puts "#{title} #{rank}"
